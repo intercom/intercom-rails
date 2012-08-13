8ff52c22b9941b1314b158b090f36f09131cf08e
Ciaran Lee <ciaran.lee@gmail.com>
2012-08-13 20:23:52 +0100

    adding support for secure mode

--- lib/intercom-rails/script_tag_helper.rb
+++ lib/intercom-rails/script_tag_helper.rb
@@ -5,13 +5,13 @@ module IntercomRails
   # Helper methods for generating Intercom javascript script tags.
   module ScriptTagHelper
     # @param user_details [Hash] a customizable hash of user details
-    # @param widget_options [Hash] an optional hash for widget customisation
+    # @param options [Hash] an optional hash for secure mode and widget customisation
     # @option user_details [String] :app_id Your application id (get it here)
     # @option user_details [String] :user_id unique id of this user within your application
     # @option user_details [String] :email email address for this user
     # @option user_details [String] :name the users name, _optional_ but useful for identify people in the Intercom App.
     # @option user_details [Hash] :custom_data custom attributes you'd like saved for this user on Intercom. See
-    # @option widget_options [String] :activator a css selector for an element which when clicked should show the Intercom widget
+    # @option options [String] :activator a css selector for an element which when clicked should show the Intercom widget
     # @return [String] Intercom script tag
     # @example basic example
     #   <%= intercom_script_tag({ :app_id => "your-app-id",
@@ -28,8 +28,12 @@ module IntercomRails
     #                             :name => current_user.name }
     #                             {:activator => "#Intercom"}
     #                          ) %>
-    def intercom_script_tag(user_details, widget_options={})
-      intercom_settings = user_details.merge({:widget => widget_options}).with_indifferent_access
+    def intercom_script_tag(user_details, options={})
+      if options[:secret]
+        secret_string = "#{options[:secret]}#{user_details[:email].blank? ? user_details[:id] : user_details[:email]}"
+        user_details[:user_hash] = Digest::SHA1.hexdigest(secret_string)
+      end
+      intercom_settings = user_details.merge({:widget => options[:widget]}).with_indifferent_access
       intercom_settings_with_dates_as_timestamps = convert_dates_to_unix_timestamps(intercom_settings)
       intercom_settings_with_dates_as_timestamps.reject! { |key, value| %w(email name user_id).include?(key.to_s) && value.nil? }
       intercom_script = <<-INTERCOM_SCRIPT

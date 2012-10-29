1de89258afa3a93ee58c3ca849b0f07bc15a0043
Ben McRedmond <ben@benmcredmond.com>
2012-10-29 15:43:19 -0700

    Remove redundant requires

--- lib/intercom-rails/railtie.rb
+++ lib/intercom-rails/railtie.rb
@@ -1,6 +1,3 @@
-require 'intercom-rails/script_tag_helper'
-require 'intercom-rails/action_controller_patch'
-
 module IntercomRails
   class Railtie < Rails::Railtie
     initializer "intercom_on_rails.script_tag_helper.rb" do |app|

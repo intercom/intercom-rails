cf1a1d12054fe79dd8abeec4a6af46f0edf7cabb
Ben McRedmond <ben@benmcredmond.com>
2012-11-14 15:44:05 -0800

    Add API Key + API secret settings to intercom config

--- lib/intercom-rails/config.rb
+++ lib/intercom-rails/config.rb
@@ -2,6 +2,7 @@ module IntercomRails
 
   module Config
 
+    # Your Intercom app_id
     def self.app_id=(value)
       @app_id = value
     end
@@ -10,6 +11,25 @@ module IntercomRails
       @app_id
     end
 
+    # Intercom api secret, for secure mode
+    def self.api_secret=(value)
+      @api_secret = value
+    end
+
+    def self.api_secret
+      @api_secret
+    end
+
+    # Intercom API key, for some rake tasks
+    def self.api_key=(value)
+      @api_key = value
+    end
+
+    def self.api_key
+      @api_key
+    end
+
+    # How is the current logged in user accessed in your controllers?
     def self.current_user=(value)
       raise ArgumentError, "current_user should be a Proc" unless value.kind_of?(Proc)
       @current_user = value
@@ -17,6 +37,16 @@ module IntercomRails
 
     def self.current_user
       @current_user
+    end
+
+    # What class defines your user model?
+    def self.user_model=(value)
+      raise ArgumentError, "user_model should be a Proc" unless value.kind_of?(Proc)
+      @user_model = value
+    end
+
+    def self.user_model
+      @user_model
     end
 
   end
--- lib/rails/generators/intercom/config/config_generator.rb
+++ lib/rails/generators/intercom/config/config_generator.rb
@@ -7,9 +7,14 @@ module Intercom
       end
 
       argument :app_id, :desc => "Your Intercom app-id, which can be found here: https://www.intercom.io/apps/api_keys"
+      argument :api_secret, :desc => "Your Intercom api-secret, used for secure mode"
+      argument :api_key, :desc => "An Intercom API key, for various rake tasks"
 
       def create_config_file
         @app_id = app_id
+        @api_secret = api_secret
+        @api_key = api_key
+
         template("intercom.rb.erb", "config/initializers/intercom.rb")
       end
 

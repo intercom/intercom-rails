4f6ca94668cabd0cd6afc0f51913fd5274157dc1
Jamie White <jamie@jgwhite.co.uk>
2013-04-03 14:02:51 +0100

    Generate unix timestamps using #to_i directly

--- lib/intercom-rails/script_tag.rb
+++ lib/intercom-rails/script_tag.rb
@@ -112,7 +112,7 @@ module IntercomRails
 
     def convert_dates_to_unix_timestamps(object)
       return Hash[object.map { |k, v| [k, convert_dates_to_unix_timestamps(v)] }] if object.is_a?(Hash)
-      return object.strftime('%s').to_i if object.respond_to?(:strftime)
+      return object.to_i if object.is_a?(Time) || object.is_a?(DateTime)
       object
     end
 

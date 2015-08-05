2029727e947caf7eb96dc914ce5e3426e4e97a8a
Francis Robichaud <frobichaud@petalmd.com>
2015-08-04 22:52:46 -0400

    Fix CustomAttributesHelper Rails dependency

--- lib/intercom-rails/railtie.rb
+++ lib/intercom-rails/railtie.rb
@@ -2,7 +2,7 @@ module IntercomRails
   class Railtie < Rails::Railtie
     initializer "intercom-rails" do |app|
       ActionView::Base.send :include, ScriptTagHelper
-      ActionController::Base.send :include, CustomDataHelper
+      ActionController::Base.send :include, CustomAttributesHelper
       ActionController::Base.send :include, AutoInclude::Method
       ActionController::Base.after_filter :intercom_rails_auto_include
     end

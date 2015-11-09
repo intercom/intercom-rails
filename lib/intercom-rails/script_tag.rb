256b019324cf60d2922fd5b09aece10955850f58
Bob Long <robertjflong@gmail.com>
2015-11-09 11:23:15 +0000

    Fix mismatch in generated script

--- lib/intercom-rails/script_tag.rb
+++ lib/intercom-rails/script_tag.rb
@@ -91,6 +91,7 @@ module IntercomRails
     end
 
     def app_id
+      return user_details[:app_id] if user_details[:app_id].present?
       return IntercomRails.config.app_id if IntercomRails.config.app_id.present?
       return 'abcd1234' if defined?(Rails) && Rails.env.development?
 
--- lib/intercom-rails/script_tag_helper.rb
+++ lib/intercom-rails/script_tag_helper.rb
@@ -33,7 +33,6 @@ module IntercomRails
       options[:find_current_user_details] = !options[:user_details]
       options[:find_current_company_details] = !(options[:user_details] && options[:user_details][:company])
       options[:controller] = controller if defined?(controller)
-
       ScriptTag.generate(options)
     end
   end

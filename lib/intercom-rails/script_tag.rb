b39b5763b8b6ba06a719193c7a1f18e48ed73074
Bob Long <robertjflong@gmail.com>
2015-11-09 10:59:29 +0000

    Escape configured app_id

--- lib/intercom-rails/script_tag.rb
+++ lib/intercom-rails/script_tag.rb
@@ -1,10 +1,13 @@
 require 'active_support/json'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/string/output_safety'
+require 'action_view'
 
 module IntercomRails
 
   class ScriptTag
+
+    include ::ActionView::Helpers::JavaScriptHelper
 
     def self.generate(*args)
       new(*args).output
@@ -48,7 +51,7 @@ module IntercomRails
 <script id="IntercomSettingsScriptTag">
   window.intercomSettings = #{intercom_settings_json};
 </script>
-<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='#{Config.library_url || "https://widget.intercom.io/widget/#{app_id}"}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})()</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='#{Config.library_url || "https://widget.intercom.io/widget/#{j app_id}"}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})()</script>
       INTERCOM_SCRIPT
 
       str.respond_to?(:html_safe) ? str.html_safe : str

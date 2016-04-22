fab6c62db984bae37e6ea953bbb727dfffd18711
Skaelv <kevin.antoine@intercom.io>
2016-04-22 12:19:53 +0100

    fix shutdown method

--- lib/intercom-rails/shutdown_helper.rb
+++ lib/intercom-rails/shutdown_helper.rb
@@ -4,8 +4,27 @@ module IntercomRails
     # It is recommanded to call this function every time a user log out of your application
     # specifically if you use both "Acquire" and another Intercom product
     # Do not use before a redirect_to because it will not clear the cookies on a redirection
-    def self.intercom_shutdown_helper (controller)
-      controller.response.delete_cookie("intercom-session-#{IntercomRails.config.app_id}")
+    def self.intercom_shutdown_helper(cookies)
+      if (cookies.is_a?(ActionDispatch::Cookies::CookieJar))
+        cookies["intercom-session-#{IntercomRails.config.app_id}"] = { value: nil, expires: 1.day.ago}
+      else
+        controller = cookies
+        Rails.logger.info("Warning: IntercomRails::ShutdownHelper.intercom_shutdown_helper takes an instance of ActionDispatch::Cookies::CookieJar as an argument since v0.2.34. Passing a controller is depreciated. See https://github.com/intercom/intercom-rails#shutdown for more details.")
+        controller.response.delete_cookie("intercom-session-#{IntercomRails.config.app_id}", { value: nil, expires: 1.day.ago})
+      end
+    rescue
     end
+
+    def self.prepare_intercom_shutdown(session)
+      session[:perform_intercom_shutdown] = true
+    end
+
+    def self.intercom_shutdown(session, cookies)
+      if session[:perform_intercom_shutdown]
+        session.delete(:perform_intercom_shutdown)
+        intercom_shutdown_helper(cookies)
+      end
+    end
+
   end
 end
--- spec/shutdown_helper_spec.rb
+++ spec/shutdown_helper_spec.rb
@@ -4,7 +4,17 @@ require 'action_controller'
 describe TestController, type: :controller do
   include IntercomRails::ShutdownHelper
   it 'clears response intercom-session-{app_id} cookie' do
-    IntercomRails::ShutdownHelper.intercom_shutdown_helper(self)
-    expect(self.response.cookies).to eq({"intercom-session-abc123"=>nil})
+    IntercomRails::ShutdownHelper.intercom_shutdown_helper(cookies)
+    expect(cookies.has_key?('intercom-session-abc123')).to eq true
+  end
+  it 'creates session[:perform_intercom_shutdown] var' do
+    IntercomRails::ShutdownHelper.prepare_intercom_shutdown(session)
+    expect(session[:perform_intercom_shutdown]).to eq true
+  end
+  it 'erase intercom cookie, set preform_intercom_shutdown sessions to nil' do
+    session[:perform_intercom_shutdown] = true
+    IntercomRails::ShutdownHelper.intercom_shutdown(session, cookies)
+    expect(session[:perform_intercom_shutdown]).to eq nil
+    expect(cookies.has_key?('intercom-session-abc123')).to eq true
   end
 end

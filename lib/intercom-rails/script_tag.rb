3e6e2d8680c8c50d0771618fe9425f10d5895c37
Pavel Rosický <pdahorek@seznam.cz>
2020-04-28 02:36:13 +0200

    [Performance] Improve memory usage and object retention (#319)
    Co-authored-by: Chris Lowder <clowder@gmail.com>
    Co-authored-by: pavel <pavel.rosicky@easy.cz>

--- lib/intercom-rails/script_tag.rb
+++ lib/intercom-rails/script_tag.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_support/json'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/string/output_safety'
@@ -6,8 +8,15 @@ require 'action_view'
 module IntercomRails
 
   class ScriptTag
+    # Base64 regexp:
+    # - blocks of 4 [A-Za-z0-9+/]
+    # followed either by:
+    # - blocks of 2 [A-Za-z0-9+/] + '=='
+    # - blocks of 3 [A-Za-z0-9+/] + '='
+    NONCE_RE = %r{^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$}
 
     include ::ActionView::Helpers::JavaScriptHelper
+    include ::ActionView::Helpers::TagHelper
 
     attr_reader :user_details, :company_details, :show_everywhere, :session_duration
     attr_accessor :secret, :widget_options, :controller, :nonce, :encrypted_mode_enabled, :encrypted_mode
@@ -46,21 +55,14 @@ module IntercomRails
       valid
     end
 
-    def valid_nonce?
-      valid = false
-      if nonce
-        # Base64 regexp:
-        # - blocks of 4 [A-Za-z0-9+/]
-        # followed either by:
-        # - blocks of 2 [A-Za-z0-9+/] + '=='
-        # - blocks of 3 [A-Za-z0-9+/] + '='
-        base64_regexp = Regexp.new('^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$')
-        m = base64_regexp.match(nonce)
-        if nonce == m.to_s
-          valid = true
-        end
+    if //.respond_to?(:match?)
+      def valid_nonce?
+        nonce && NONCE_RE.match?(nonce)
       end
-      valid
+    else
+      def valid_nonce?
+        nonce && !!NONCE_RE.match(nonce)
+      end
     end
 
     def intercom_settings
@@ -73,12 +75,9 @@ module IntercomRails
     end
 
     def to_s
-      js_options = 'id="IntercomSettingsScriptTag"'
-      if nonce && valid_nonce?
-        js_options = js_options + " nonce=\"#{nonce}\""
-      end
-      str = "<script #{js_options}>#{intercom_javascript}</script>\n"
-      str.respond_to?(:html_safe) ? str.html_safe : str
+      html_options = { id: 'IntercomSettingsScriptTag' }
+      html_options['nonce'] = nonce if valid_nonce?
+      javascript_tag(intercom_javascript, html_options) + "\n"
     end
 
     def csp_sha256
@@ -111,9 +110,7 @@ module IntercomRails
       plaintext_javascript = ActiveSupport::JSON.encode(plaintext_settings).gsub('<', '\u003C')
       intercom_encrypted_payload_javascript = encrypted_mode.encrypted_javascript(intercom_settings)
 
-      str = "window.intercomSettings = #{plaintext_javascript};#{intercom_encrypted_payload_javascript}(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='#{Config.library_url || "https://widget.intercom.io/widget/#{j app_id}"}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})()"
-
-      str
+      "window.intercomSettings = #{plaintext_javascript};#{intercom_encrypted_payload_javascript}(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='#{Config.library_url || "https://widget.intercom.io/widget/#{j app_id}"}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})()"
     end
 
     def user_details=(user_details)
